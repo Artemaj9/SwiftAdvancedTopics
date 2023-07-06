a1383bc5764e12acbb0fec52e9d5ec7baf114492
Artem <moiseev@physics.msu.ru>
2023-07-06 19:01:47 +0300

    Initial Commit

---
+++ SwiftAdvancedTopics/SwiftAdvancedTopicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAdvancedTopicsApp.swift
+//  SwiftAdvancedTopics
+//
+//  Created by Artem on 06.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAdvancedTopicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
